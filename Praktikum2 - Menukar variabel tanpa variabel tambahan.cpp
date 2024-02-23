de998d74e71f1f824a93adfe155b2268c8fed404
Nicholas Jeremy <145427187+Senicccc@users.noreply.github.com>
2024-02-23 11:38:53 +0700

    Update and rename Menukar variabel tanpa variabel tambahan.cpp to Praktikum2 - Menukar variabel tanpa variabel tambahan.cpp

---
+++ Praktikum2 - Menukar variabel tanpa variabel tambahan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    //memasukan variabel yang akan ditukar
+    cout <<"Variabel Pertama :";cin>>a;
+    cout <<"Variabel Kedua :";cin>>b;
+
+    //untuk output variabel setelah ditukar
+    cout <<"Variabel Pertama :"<<b<<endl;
+    cout <<"Variabel Kedua:"<<a<<endl;
+    // tinggal dimasukan saja dimana variabel pertama itu 10 dan variabel kedua itu 8 yang nantinya akan ditukar
+}
