de2ec9e216e83db0e95b3e427c65839cf11c434f
Nicholas Jeremy <145427187+Senicccc@users.noreply.github.com>
2024-02-23 11:35:10 +0700

    Update and rename main.cpp to Praktikum1.cpp

---
+++ Praktikum1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+// input output stream
+
+using namespace std;
+int main()
+{
+    int length = 120;
+   std::cout << std::string(length, '-') << '\n';
+
+    cout << "Nama: Nicholas Jeremy Hendrajaya\n\n";
+    cout << "NPM: 231\n\n";
+    cout << "Kelas:A - Informatika\n\n";
+    cout << "Cita-Cita: Full Stack Developer\n\n";
+    cout << "Motivasi Masuk Informatika: Prospek kerja yang luas\n\n";
+
+    std::cout << std::string(length, '-') << '\n';
+
+}
+//std::cout<<""<<std::endl; jika mau tanpa using namespace
+//cout = c output, endl = end line
